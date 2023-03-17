99cc7f4efe6281d0e0dcbb820dadce690849cebf
Lukas Schwab <lukas.schwab@gmail.com>
2023-03-17 11:48:47 -0700

    Do not test specific distance measure

--- hcl_test.go
+++ hcl_test.go
@@ -8,12 +8,8 @@ import (
 )
 
 func TestDistanceSquared(t *testing.T) {
-	a := forceHCL(color.RGBA{0, 0, 0, 255})
-	b := forceHCL(color.RGBA{255, 255, 255, 255})
-	assert.InDelta(t, 1, a.distanceSquared(b), .0001, "distance should be square of Euclidean distance")
-
-	a = forceHCL(color.RGBA{0, 0, 0, 1})
-	b = forceHCL(color.RGBA{0, 0, 0, 255})
+	a := forceHCL(color.RGBA{0, 0, 0, 1})
+	b := forceHCL(color.RGBA{0, 0, 0, 255})
 	assert.Equal(t, 0.00, a.distanceSquared(b), "alpha channel should be ignored for the purpose of distance")
 
 	c := forceHCL(randomColor())
